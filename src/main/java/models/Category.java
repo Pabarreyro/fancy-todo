148b9040e1e557063e05f25a6ae13519b99aa139
Pablo Barreyro <pabarreyro@gmail.com>
2018-06-07 08:37:08 -0700

    Add passing code for instantiating Category object, with getters, setters, equals(), hashCode()

--- src/main/java/models/Category.java
+++ src/main/java/models/Category.java
@@ -6,9 +6,7 @@ public class Category {
    private String name;
    private int id;
 
-    public Category(String name) {
-        this.name = name;
-    }
+    public Category(String name) { this.name = name; }
 
     public String getName() { return name; }
 
