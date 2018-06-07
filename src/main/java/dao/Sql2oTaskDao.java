f721fb8f2e8543296952ee27456c60f833f8d7cf
Pablo Barreyro <pabarreyro@gmail.com>
2018-06-07 11:46:21 -0700

    Add passing logic for add(), getAll(), findById() methods

--- src/main/java/dao/Sql2oTaskDao.java
+++ src/main/java/dao/Sql2oTaskDao.java
@@ -1,11 +1,63 @@
 package dao;
 
+import models.Task;
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
-public class Sql2oTaskDao implements taskDao {
+import java.util.List;
+
+public class Sql2oTaskDao implements TaskDao {
     private final Sql2o sql2o;
 
     public Sql2oTaskDao(Sql2o sql2o) { this.sql2o = sql2o; }
 
     @Override
+    public void add(Task task){
+        String sql = "INSERT INTO tasks (description, categoryId) VALUES (:description, :categoryId)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .bind(task)
+                    .executeUpdate()
+                    .getKey();
+            task.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public List<Task> getAll() {
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM tasks")
+                    .executeAndFetch(Task.class);
+        }
+    }
+
+    @Override
+    public Task findById(int id) {
+        String sql = "SELECT * FROM tasks WHERE id=:id";
+        try (Connection con = sql2o.open()) {
+            return con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Task.class);
+        }
+    }
+
+    @Override
+    public void update(int id, String name, int categoryId) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
+
+
 }
