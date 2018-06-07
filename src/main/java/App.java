ebd05438e50e2b6f68af54659da817652c68c926
Pablo Barreyro <pabarreyro@gmail.com>
2018-06-07 14:03:29 -0700

    Set up routing paths

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,84 +2,76 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import dao.CategoryDao;
+import dao.Sql2oCategoryDao;
+import dao.Sql2oTaskDao;
+import dao.TaskDao;
 import models.Task;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
+
     public static void main(String[] args) { //type “psvm + tab” to autocreate this
         staticFileLocation("/public");
 
-        //get: delete all tasks
-        get("/tasks/delete", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            Task.clearAllTasks();
-            res.redirect("/");
-            return null;
-        }, new HandlebarsTemplateEngine());
+        String connectionString = "jdbc:h2:~/todolist.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        CategoryDao categoryDao = new Sql2oCategoryDao(sql2o);
+        TaskDao taskDao = new Sql2oTaskDao(sql2o);
+        Connection conn = sql2o.open();
 
-        //get: delete an individual task
-        get("/tasks/:id/delete", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            int idOfTaskToDelete = Integer.parseInt(req.params("id")); //pull id - must match route segment
-            Task deleteTask = Task.findById(idOfTaskToDelete); //use it to find task
-            deleteTask.deleteTask();
-            res.redirect("/");
-            return null;
-        }, new HandlebarsTemplateEngine());
+        // get: display all categories and tasks therein
+        // /
 
-        //get: show all tasks
-        get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            ArrayList<Task> tasks = Task.getAll();
-            model.put("tasks", tasks);
-            return new ModelAndView(model, "index.hbs");
-        }, new HandlebarsTemplateEngine());
+        // get: display new category form (display all categories)
+        // /categories/new
 
-        //get: show new task form
-        get("/tasks/new", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            return new ModelAndView(model, "task-form.hbs");
-        }, new HandlebarsTemplateEngine());
+        // post: submit category form (redirect to /)
+        // /categories
 
-        //task: process new task form
-        post("/tasks", (req, res) -> { //URL to make new task on POST route
-            Map<String, Object> model = new HashMap<>();
-            String description = req.queryParams("description");
-            Task newTask = new Task(description);
-            res.redirect("/");
-            return null;
-        }, new HandlebarsTemplateEngine());
+        // get: display single category by id, with all related tasks (display all categories)
+        // /categories/:id
 
-        //get: show an individual task
-        get("/tasks/:id", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            int idOfTaskToFind = Integer.parseInt(req.params("id")); //pull id - must match route segment
-            Task foundTask = Task.findById(idOfTaskToFind); //use it to find task
-            model.put("task", foundTask); //add it to model for template to display
-            return new ModelAndView(model, "task-detail.hbs"); //individual task page.
-        }, new HandlebarsTemplateEngine());
+        // get: display update form for single category by id (display all categories)
+        // /categories/:id/update
 
-        //get: show a form to update a task
-        get("/tasks/:id/update", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            int idOfTaskToEdit = Integer.parseInt(req.params("id"));
-            Task editTask = Task.findById(idOfTaskToEdit);
-            model.put("editTask", editTask);
-            return new ModelAndView(model, "task-form.hbs");
-        }, new HandlebarsTemplateEngine());
+        // post: submit category update form (redirect to /categories/:id)
+        // /categories
 
-        //task: process a form to update a task
-        post("/tasks/:id", (req, res) -> { //URL to update task on POST route
-            Map<String, Object> model = new HashMap<>();
-            String newContent = req.queryParams("description");
-            int idOfTaskToEdit = Integer.parseInt(req.params("id"));
-            Task editTask = Task.findById(idOfTaskToEdit);
-            editTask.update(newContent);
-            res.redirect("/");
-            return null;
-        }, new HandlebarsTemplateEngine());
+        // get: display new task form for single category by id (display all categories)
+        // /categories/:id/task/new
+
+        // post: submit new task to single category (redirect to /categories/:id)
+        // /categories/:id/task
+
+        // get: display new task form (display all categories)
+        // /tasks/new
+
+        // post: submit new task form (redirect to /)
+        // /tasks
+
+        // get: display task update form (display all categories)
+        // /tasks/:id/update
+
+        // post: submit task update form (redirect to /categories/:id/tasks)
+        // /tasks
+
+        // post: delete category by id & all related tasks (redirect to /)
+        // /categories/:id/delete
+
+        // post: delete all categories & tasks (redirect to /)
+        // /categories/delete
+
+        // post: delete task by id (redirect to /categories/:id)
+        // /tasks/:id/delete
+
+        // post: delete all tasks (redirect to /)
+        // /tasks/delete
+
 
     }
 }
