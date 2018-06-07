72e448a26c72824e9516f315f7d719a8171fc418
Pablo Barreyro <pabarreyro@gmail.com>
2018-06-07 10:08:41 -0700

    Add test for add() method

---
+++ src/test/java/dao/Sql2oCategoryDaoTest.java
@@ -0,0 +1,37 @@
+package dao;
+
+import models.Category;
+import org.junit.*;
+import org.sql2o.*;
+
+import static org.junit.Assert.*;
+
+public class Sql2oCategoryDaoTest {
+    private Sql2oCategoryDao categoryDao;
+    private Connection conn;
+
+    @Before
+    public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        categoryDao = new Sql2oCategoryDao(sql2o);
+        conn = sql2o.open();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        conn.close();
+    }
+
+    @Test
+    public void add_addsNewCategoryCorrectly() {
+        Category testCategory = setupCategory();
+        int initialId = testCategory.getId();
+        categoryDao.add(testCategory);
+        assertNotEquals(initialId, testCategory.getId());
+    }
+
+    public Category setupCategory() {
+        return new Category("Yardwork");
+    }
+}
